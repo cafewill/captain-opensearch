5c0b55511241c01de999036f357791327744e41f
Bill <min@cafewill.com>
2026-04-29 07:01:22 +0900

    feat: add spring boot sample (java8 ~ 25).

---
+++ simple-jobs-spring-maven-with-java17/src/main/java/com/cube/simple/opensearch/AbstractOpenSearchAppender.java
@@ -0,0 +1,196 @@
+package com.cube.simple.opensearch;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.cube.simple.opensearch.config.Authentication;
+import com.cube.simple.opensearch.config.OpenSearchProperties;
+import com.cube.simple.opensearch.config.HttpRequestHeaders;
+import com.cube.simple.opensearch.config.Operation;
+import com.cube.simple.opensearch.config.Settings;
+import com.cube.simple.opensearch.util.ErrorReporter;
+
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
+
+public abstract class AbstractOpenSearchAppender<T> extends UnsynchronizedAppenderBase<T> {
+
+    protected Settings settings;
+    protected OpenSearchProperties opensearchProperties;
+    protected AbstractOpenSearchPublisher<T> publisher;
+    protected ErrorReporter errorReporter;
+    protected HttpRequestHeaders headers;
+
+    public AbstractOpenSearchAppender() {
+        this.settings = new Settings();
+        this.headers = new HttpRequestHeaders();
+        registerShutdownHook();
+    }
+
+    public AbstractOpenSearchAppender(Settings settings) {
+        this.settings = settings;
+        registerShutdownHook();
+    }
+
+    private void registerShutdownHook() {
+        Runtime.getRuntime().addShutdownHook(new Thread(new ShutdownHook()));
+    }
+
+    private class ShutdownHook implements Runnable {
+        @Override
+        public void run() {
+            stop();
+            if (publisher != null) {
+                publisher.close();
+            }
+        }
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        this.errorReporter = getErrorReporter();
+        try {
+            this.publisher = buildOpenSearchPublisher();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected void publishEvent(T eventObject) {
+        publisher.addEvent(eventObject);
+    }
+
+    //VisibleForTesting
+    protected ErrorReporter getErrorReporter() {
+        return new ErrorReporter(settings, getContext());
+    }
+
+    //VisibleForTesting
+    protected abstract AbstractOpenSearchPublisher<T> buildOpenSearchPublisher() throws IOException;
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+
+    @Override
+    protected void append(T eventObject) {
+        appendInternal(eventObject);
+    }
+
+    protected abstract void appendInternal(T eventObject);
+
+    public void setProperties(OpenSearchProperties opensearchProperties) {
+        this.opensearchProperties = opensearchProperties;
+    }
+
+    public void setSleepTime(int sleepTime) {
+        settings.setSleepTime(sleepTime);
+    }
+
+    public void setMaxRetries(int maxRetries) {
+        settings.setMaxRetries(maxRetries);
+    }
+
+    public void setConnectTimeout(int connectTimeout) {
+        settings.setConnectTimeout(connectTimeout);
+    }
+
+    public void setReadTimeout(int readTimeout) {
+        settings.setReadTimeout(readTimeout);
+    }
+
+    public void setIncludeCallerData(boolean includeCallerData) {
+        settings.setIncludeCallerData(includeCallerData);
+    }
+
+    public void setErrorsToStderr(boolean errorsToStderr) {
+        settings.setErrorsToStderr(errorsToStderr);
+    }
+
+    public void setLogsToStderr(boolean logsToStderr) {
+        settings.setLogsToStderr(logsToStderr);
+    }
+
+    public void setMaxQueueSize(int maxQueueSize) {
+        settings.setMaxQueueSize(maxQueueSize);
+    }
+
+    public void setIndex(String index) {
+        settings.setIndex(index);
+    }
+
+    public void setType(String type) {
+        settings.setType(type);
+    }
+
+    public void setUrl(String url) throws MalformedURLException {
+        settings.setUrl(new URL(url));
+    }
+
+    public void setLoggerName(String logger) {
+        settings.setLoggerName(logger);
+    }
+
+    public void setErrorLoggerName(String logger) {
+        settings.setErrorLoggerName(logger);
+    }
+
+    public void setHeaders(HttpRequestHeaders httpRequestHeaders) {
+        this.headers = httpRequestHeaders;
+    }
+
+    public void setRawJsonMessage(boolean rawJsonMessage) {
+        settings.setRawJsonMessage(rawJsonMessage);
+    }
+    public void setTimestampFormat(String timestampFormat) {
+        settings.setTimestampFormat(timestampFormat);
+    }
+
+    public void setIncludeMdc(boolean includeMdc) {
+        settings.setIncludeMdc(includeMdc);
+    }
+
+    public void setAuthentication(Authentication auth) {
+        settings.setAuthentication(auth);
+    }
+
+    public void setMaxMessageSize(int maxMessageSize) {
+        settings.setMaxMessageSize(maxMessageSize);
+    }
+
+    public void setKeyPrefix(String keyPrefix) {
+        settings.setKeyPrefix(keyPrefix);
+    }
+
+    public void setObjectSerialization(boolean objectSerialization) {
+        settings.setObjectSerialization(objectSerialization);
+    }
+
+    public void setAutoStackTraceLevel(String level) {
+        settings.setAutoStackTraceLevel(level);
+    }
+
+    public void setOperation( String operation ) {
+        settings.setOperation( Operation.of( operation )
+                .orElseGet(
+                    () -> {
+                        addWarn( "Invalid value provided for [operation] setting, assuming create" );
+                        return Operation.create;
+                    } )
+            );
+    }
+
+    public void setIncludeKvp(boolean includeKvp) {
+      settings.setIncludeKvp(includeKvp);
+    }
+
+    public void setMaxBatchSize(int maxBatchSize) {
+        settings.setMaxBatchSize(maxBatchSize);
+    }
+
+    public void setTrustAllSsl(boolean trustAllSsl) {
+        settings.setTrustAllSsl(trustAllSsl);
+    }
+}
